a6fc548700a5220fe64c09525e730075f6523cbf
KingKittyTurnip <128203370+KingKittyTurnip@users.noreply.github.com>
2025-02-05 05:14:05 -0600

    1.7ified 1.8
    knockback iframes are 2 seconds instead of 1
    ice pickup delay is now 5 frames instead of 25

--- Assets/QuantumUser/Simulation/NSMB/Entity/Player/MarioPlayer.cs
+++ Assets/QuantumUser/Simulation/NSMB/Entity/Player/MarioPlayer.cs
@@ -65,7 +65,7 @@ namespace Quantum {
             if (f.Unsafe.TryGetPointer(item, out Holdable* holdable)) {
                 aboveHead = holdable->HoldAboveHead;
                 if (aboveHead) {
-                    forceHold = (f.Number - HoldStartFrame) < 25;
+                    forceHold = (f.Number - HoldStartFrame) < 5; //25;
                 }
             }
 
@@ -449,7 +449,7 @@ namespace Quantum {
         public void ResetKnockback(Frame f, EntityRef entity) {
             var physicsObject = f.Unsafe.GetPointer<PhysicsObject>(entity);
             KnockbackGetupFrames = (byte) (IsInWeakKnockback || physicsObject->IsUnderwater ? 0 : 25);
-            DamageInvincibilityFrames = (byte) (60 + KnockbackGetupFrames);
+            DamageInvincibilityFrames = (byte) (120 + KnockbackGetupFrames);
             ////DoEntityBounce = false;
             IsInKnockback = false;
             IsInWeakKnockback = false;
